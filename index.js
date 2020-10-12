b432ddf26bd4ce1aad22b5df4daae101565aa8e7
Duc-Thomas <43140281+Duckiduc@users.noreply.github.com>
2020-10-13 00:50:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 // index.js
 const Mustache = require('mustache');
-const fetch = require('node-fetch');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
 let DATA = {
     name: 'Duc-Thomas',
-    date: new Date().toLocaleDateString('en-GB', {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        timeZoneName: 'short',
-        timeZone: 'Europe/Paris',
-    }),
 };
 
 function generateReadMe() {
@@ -33,4 +23,3 @@ async function action() {
 }
   
 action();
-  
