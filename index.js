d2a32ae792d1321f90b121745c74e1d81885f68c
Duc-Thomas Nguyen <duckiduc.dev@gmail.com>
2020-10-13 00:18:07 +0200

    Let's go

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // index.js
+require('dotenv').config();
 const Mustache = require('mustache');
+const fetch = require('node-fetch');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
@@ -16,6 +18,28 @@ let DATA = {
     }),
 };
 
+async function setWeatherInformation() {
+    await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${secrets.OPEN_WEATHER_MAP_KEY}&units=metric`
+    )
+    .then(r => r.json())
+    .then(r => {
+        DATA.city_temperature = Math.round(r.main.temp);
+        DATA.city_weather = r.weather[0].description;
+        DATA.city_weather_icon = r.weather[0].icon;
+        DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+            hour: '2-digit',
+            minute: '2-digit',
+            timeZone: 'Europe/Stockholm',
+        });
+        DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+            hour: '2-digit',
+            minute: '2-digit',
+            timeZone: 'Europe/Paris',
+        });
+    });
+}
+
 function generateReadMe() {
     fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
         if (err) throw err;
@@ -23,4 +47,18 @@ function generateReadMe() {
         fs.writeFileSync('README.md', output);
     });
 }
-generateReadMe();
+
+async function action() {
+    /**
+     * Fetch Weather
+     */
+    await setWeatherInformation();
+  
+    /**
+     * Generate README
+     */
+    await generateReadMe();
+}
+  
+action();
+  
